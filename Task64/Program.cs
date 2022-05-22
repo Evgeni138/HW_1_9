4dc77c563ede365a197c3c61fc9dc824efdd47fd
Evgeni <666997@bk.ru>
2022-05-22 09:33:53 +0800

    Task64

---
+++ Task64/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 64: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+
+
+int SumNumber(int num, int sum = 0)
+{
+    if(num > 0)
+    {
+        sum = sum + num % 10;
+        num /= 10;
+        return SumNumber(num, sum);
+    }
+    else return sum;
+}
+
+Console.WriteLine(SumNumber(453));
